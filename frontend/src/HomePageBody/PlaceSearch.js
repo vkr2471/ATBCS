5fef592628c94c6b585642fa29181ffc4191db1f
MokshithPV <venkatsaimokshithp@gmail.com>
2023-03-22 18:26:03 +0530

    fixed warning

--- frontend/src/HomePageBody/PlaceSearch.js
+++ frontend/src/HomePageBody/PlaceSearch.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 export default function PlaceSearch() {
+    const retur = () => {
+        document.getElementById('return').disabled = false;
+    };
+    const retu = () => {
+        document.getElementById('return').disabled = true;
+    }
     return (
         <div>
-            <p></p>
-            <ul className='body--inputs'>
-                <li><p>From<input type='text' placeholder='Source' className='input'/></p></li>
-                <li><p>To<input type='text' placeholder='Destination' className='input'/></p></li>
-            </ul>
+            <form className='form'>
+                <div className='options'>
+                    <input type='radio' name='option' id='one' value='one-way' onClick={retu}/>
+                    <label>One Way</label>
+                    <input type='radio' name='option' id='round' value='round-trip' onClick={retur}/>
+                    <label>Round Trip</label>
+                </div>
+                <input type="text" placeholder="Source" />
+                <input type="text" placeholder="Destination" />
+                <input type="date" placeholder="Departure Date" />
+                <input type="date" placeholder="Return Date" id='return'  disabled />
+            </form>
         </div>
     );
 }
