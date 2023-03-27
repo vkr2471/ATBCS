fa29266b658401755839c746e4e9bbbe66d8f519
Bhavani Kalal <104501835+Bhavani-Monu@users.noreply.github.com>
2023-03-27 16:21:03 +0530

    changed logout

--- frontend/src/Template/logout.js
+++ frontend/src/Template/logout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Logout() {
     return (
-        <div>
-            <h1>Are you sure you want to logout</h1>
-            <Link to={`/yes/${localStorage.getItem('user')}`}>Yes</Link>
-            <Link to={"/"}>No</Link>
+        <div className="logout-header">
+            <h1 >Are you sure you want to logout ?</h1>
+            <div className="logout-options">
+                <Link to={`/yes/${localStorage.getItem('user')}`} ><button className="logout-yes">Yes</button></Link>
+                <Link to={"/"} ><button className="logout-no">No</button></Link>
+            </div>
         </div>
     );
 }
