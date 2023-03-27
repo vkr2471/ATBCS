af92ff9e84738794d87a65f5f9c5b5b644abaf2c
Bhavani Kalal <104501835+Bhavani-Monu@users.noreply.github.com>
2023-03-27 14:32:57 +0530

    changed home

---
+++ frontend/src/FlightSchedule/Flight.js
@@ -0,0 +1,111 @@
+import React, { useEffect } from 'react';
+import "./Flight.css";
+import Details from './Details';
+export default function Flight() {
+    const [dataTable, setDataTable] = React.useState([]);
+    const data=[
+        {
+            "_id": {
+              "$oid": "641b4bbd92c312efc9ec8e06"
+            },
+            "date": "2023-04-28",
+            "flights": [
+              {
+                "flightid": "KB0194",
+                "source": "AHMEDABAD",
+                "destination": "DELHI",
+                "departure": "06:00 AM",
+                "arrival": "07:45 AM",
+                "totalseats": {
+                  "fc": 6,
+                  "bc": 12,
+                  "ec": 107
+                },
+                "seatsbooked": {
+                  "fc": 0,
+                  "bc": 0,
+                  "ec": 0
+                },
+                "ticketfare": {
+                  "fc": 59088,
+                  "bc": 19696,
+                  "ec": 2462
+                },
+                "_id": {
+                  "$oid": "641b4bbd92c312efc9ec8e07"
+                }
+              },
+              {
+                "flightid": "KB2971",
+                "source": "AHMEDABAD",
+                "destination": "JAIPUR",
+                "departure": "09:25 AM",
+                "arrival": "10:55 AM",
+                "totalseats": {
+                  "fc": 7,
+                  "bc": 15,
+                  "ec": 137
+                },
+                "seatsbooked": {
+                  "fc": 0,
+                  "bc": 0,
+                  "ec": 0
+                },
+                "ticketfare": {
+                  "fc": 49848,
+                  "bc": 16616,
+                  "ec": 2077
+                }
+            },
+                {
+                    "flightid": "KB0491",
+                    "source": "AHMEDABAD",
+                    "destination": "BAGDOGRA",
+                    "departure": "06:10 AM",
+                    "arrival": "08:40 AM",
+                    "totalseats": {
+                    "fc": 6,
+                    "bc": 13,
+                    "ec": 111
+                    },
+                    "seatsbooked": {
+                    "fc": 0,
+                    "bc": 0,
+                    "ec": 0
+                    },
+                    "ticketfare": {
+                    "fc": 104040,
+                    "bc": 34680,
+                    "ec": 4335
+                    },
+              }
+            ]
+        }
+    ]
+    const date = data[0]["date"];
+    return (
+        
+        <div className='flights'>
+            <h2 className='heading'>Flights</h2>
+            <div className='flightsContainer'>
+                <table>
+                <thead>
+                    <tr>
+                        <th>Date </th>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>FlightID</th>
+                        <th>Departure</th>
+                        <th>Arrival</th>
+                        <th>Fees</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data[0]["flights"].map((item,index)=><Details item={item} date={date}/>)}
+                </tbody>
+            </table>
+            </div>
+
+        </div>
+    );
+}
--- frontend/src/HomePageBody/PlaceSearch.js
+++ frontend/src/HomePageBody/PlaceSearch.js
@@ -8,28 +8,69 @@ export default function PlaceSearch() {
         document.getElementById('return').disabled = true;
     }
     return (
-        <div>
+        <div className='wrapper'>
             <form className='form'>
+                <h2 className='title'>Search Flights</h2>
                 <div className='options'>
                     <input type='radio' name='option' id='one' value='one-way' onClick={retu}/>
                     <label>One Way</label>
                     <input type='radio' name='option' id='round' value='round-trip' onClick={retur}/>
                     <label>Round Trip</label>
                 </div>
-                <fieldset className="place">
-                    <legend>From</legend>
-                    <input type="text" placeholder="City or Airport" className = 'textbox' />
-                </fieldset>
-                <fieldset className='place'>
-                    <legend>To</legend>
-                    <input type="text" placeholder="City or Airport" className = 'textbox' />
-                </fieldset>
-                <fieldset className='time'>
-                    <legend className='time-header'>Travel date</legend>
-                    <input type="date" placeholder="Departure Date" className='date'/>
-                    <p>-</p>
-                    <input type="date" placeholder="Return Date" id='return' className='date' disabled />
-                </fieldset>
+                <div className='search-place'>
+                    <fieldset className="place">
+                        <legend>From</legend>
+                        <input type="text" placeholder="City or Airport" className = 'textbox' />
+                    </fieldset>
+                    <fieldset className='place'>
+                        <legend>To</legend>
+                        <input type="text" placeholder="City or Airport" className = 'textbox' />
+                    </fieldset>
+                </div>
+                <label className='passenger'>Passengers</label>
+                <div className='age'>
+                    <div className='adults-age'>
+                    <label className='adults'>Adults (greater than 15)</label>
+                    <select className='adults'>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>    
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select></div>
+                    <div className='children-age'>
+                        <label className='children'>Children (2-15)</label>
+                        <select className='children'>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </div>
+                    <div className='infants-age'>
+                    <label className='infants'>Infants(0-2)</label>
+                    <select className='infants'>
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                    </div>
+                </div>
+                <div className='choose-time'>
+                    <div className='time'>
+                        <label className='time-header'>Departure</label>
+                        <input type="date" placeholder="Departure Date" className='date'/>
+                    </div>
+                    <div className='time'>
+                        <label className='time-header'>Arrival</label>
+                        <input type="date" placeholder="Return Date" id='return' className='date' disabled />
+                    </div>
+                </div>
+                <button className='search-button'>Show Available Flights</button>
             </form>
         </div>
     );
