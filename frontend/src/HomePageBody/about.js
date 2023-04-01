deb582c60e3938d308dc428c7caa2cbc9173ec66
Manoj <100463092+CodeWithManojkumar@users.noreply.github.com>
2023-04-01 16:33:01 +0530

    first-commit

--- frontend/src/HomePageBody/about.js
+++ frontend/src/HomePageBody/about.js
@@ -7,23 +7,37 @@ import "./about.css";
 
 export default function About() {
   const { airport } = useContext(UserProvider);
+  airport.sort(function (a, b) {
+    var x = a.city < b.city ? -1 : 1;
+    return x;
+  });
   return (
     <div className="about">
       <div className="about-us">
         <h1 className="header">About us</h1>
-        <p className="content">Of all the books in the world, the best stories are found between the pages of passport. We will help you create the best of them.</p>
-        <p className="content">We are a team of travel enthusiasts who want to make your travel experience as smooth as possible. We are here to help you find the best flight deals and to make your travel experience as smooth as possible.</p>
+        <p className="content">
+          Of all the books in the world, the best stories are found between the
+          pages of passport. We will help you create the best of them.
+        </p>
+        <p className="content">
+          We are a team of travel enthusiasts who want to make your travel
+          experience as smooth as possible. We are here to help you find the
+          best flight deals and to make your travel experience as smooth as
+          possible.
+        </p>
       </div>
       <h2 className="services">Our Services:</h2>
       <p className="services-text">We operate in the following airports</p>
       <ul className="service-list">
         {airport.map((airports) => {
-          return <li key={airports._id} className="airports">{airports.city}</li>;
+          return (
+            <li key={airports._id} className="airports">
+              {airports.city}
+            </li>
+          );
         })}
       </ul>
-      <ul>
-        
-      </ul>
+      <ul></ul>
     </div>
   );
 }
